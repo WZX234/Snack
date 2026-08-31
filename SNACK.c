b062ee78c2f1c5584c4d5d810decee4a13728ce9
WZX234 <www1234681@qq.com>
2026-08-31 19:46:19 +0800

    Add files via upload

--- SNACK.c
+++ SNACK.c
@@ -1,250 +1,322 @@
-#include<time.h>
-#include<stdio.h>
-#include<conio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<windows.h>
+#include <time.h>    //for srand()
+#include <stdio.h>   //for input and output
+#include <conio.h>   //for getch() and kbhit()
+#include <stdlib.h>  //for rand(), system(), exit() and srand()
+#include <string.h>  //for strcat() and sprintf()
+#include <windows.h> //for Sleep(), SetConsoleCursorPosition() and GetStdHandle()
 
-//settings
-#define FOODS 16
-#define SLEEP_TIME 300
+//===========================macro definitions===================================
+#define FOODS 16        //quantity of food
+#define SLEEP_TIME 300  //pause time
 
-void init (void);
-void make_food (void);
-char get_drct(void);
-int run (int tdrct);
-int not_drct (int a);
-void draw_screen (void);
-int is_play (void);
+//===========================function declarations===================================
+void init        (void)     ;    //initialize the game
+void span_food   (void)     ;    //spawn food
+char get_dir     (void)     ;    //get the direction of snack
+int  run         (int dir)  ;    //run one frame
+int  opp_dir     (int dir)  ;    //get the opposite direction of snack
+void redr_screen (void)     ;    //redraw screen
+int  cont_play   (void)     ;    //continue playing ?
 
-typedef struct
+//===========================type definitions===================================
+enum //snack state
 {
-	unsigned int about: 2;    //0:NULL, 1:snack, 2:food
-	unsigned int drct : 2;
+	WELL = 0,
+	GETED,
+	OVER = 4
+};
+
+enum //snack direction
+{
+	UP = 0,
+	RIGHT,
+	DOWN,
+	LEFT
+};
+
+enum //snack type
+{
+	NULL_ = 0,
+	SNACK,
+	FOOD
+};
+
+typedef struct //snack body
+{
+	unsigned int about: 2;  //about: 0-null, 1-snack, 2-food
+	unsigned int dir  : 2;  //direction: 0-up, 1-right, 2-down, 3-left
 }snack_body;
 
-typedef struct
+typedef struct //snack head and tail
 {
-	unsigned int x: 4;
-	unsigned int y: 5;
+	unsigned int x: 4;  //x coordinate
+	unsigned int y: 5;  //y coordinate
 }where;
 
-int slong;
-char buffer[643];
-int mem_drct;
-snack_body snack[16][32];
-where head, tear;
+//===========================global variables===================================
+int snack_len;             //length of snack
+char buffer[643];          //screen buffer
+int last_dir;              //previous direction
+snack_body snack[16][32];  //snack body
+where head, tail;          //snack head and tail
 
 int main (void)
 {
+	//welcome screen
 	puts("=================================");
 	puts("|| This is a game called SNACK ||");
 	puts("=================================");
 	puts("\nControl:");
 	puts(" W : up\n A : left\n S : down\n D : right");
 	puts("\nPress any key to start:");
+
+	//wait for key press
 	while (kbhit())
 	    Sleep(100);
 	while (!kbhit())
 	    Sleep(100);
-	int is_over, next_drct;
-	_init:
-		init();
-		while(slong < 512)
+	
+	int shack_state, next_dir;
+	_init: //label for re-initialization
+		init(); //initialize the game
+
+//===========================main game loop===================================
+
+		while(snack_len < 512)  //run until the snack length reaches 512
 		{
-			Sleep(SLEEP_TIME);
-			switch(get_drct())
+			Sleep(SLEEP_TIME);  //sleep for a while
+			switch(get_dir())   //process the input direction
 			{
 				case('W'):
 				case('w'):
-					next_drct = 0;
+					next_dir = UP;
 					break;
 				case('S'):
 				case('s'):
-					next_drct = 2;
+					next_dir = DOWN;
 					break;
 				case('A'):
 				case('a'):
-					next_drct = 3;
+					next_dir = LEFT;
 					break;
 				case('D'):
 				case('d'):
-					next_drct = 1;
+					next_dir = RIGHT;
 					break;
 				default:
-					next_drct = mem_drct;
+					next_dir = last_dir;
 					break;
 			}
-			if (not_drct(next_drct) == mem_drct)
-			    next_drct = mem_drct;
-			is_over = run(next_drct);
-			if (is_over == 4)
+			
+			//prevent the snack from moving in the opposite direction
+			if (opp_dir(next_dir) == last_dir)
+			    next_dir = last_dir;
+			shack_state = run(next_dir);
+
+			//check the state of the snack
+			if (shack_state == OVER)
 			{
-				if (is_play())
+				//if the player wants to continue playing, re-initialize the game
+				if (cont_play())
 					goto _init;
-				return 0;
+				//else, exit the game
+				return 0; 
 			}
-			draw_screen();
-			if (is_over)
+			redr_screen();
+			//if the snack has eaten food, play a sound
+			if (shack_state == GETED)
 				putchar('\a');
 		}
 		puts("YOU WIN! ");
-		if (is_play())
+		//if the player wants to continue playing, re-initialize the game
+		if (cont_play())
 			goto _init;
+		//else, exit the game
 		return 0;
 }
 
+//===========================function definitions===================================
 void init (void)
 {
-	system("cls");
-	slong = 3;
-	mem_drct = 0;
-	head.x = 10; head.y = 16;
-	tear.x = 12; tear.y = 16;
+	system("cls"); //clear the screen
+	snack_len = 3; //initialize the length of the snack
+	last_dir = UP; //initialize the previous direction
+	head.x = 10; head.y = 16; //initialize the head position
+	tail.x = 12; tail.y = 16; //initialize the tail position
+
+	//mould a shack
 	for (int i = 0; i < 16; i++)
 	    for (int j = 0; j < 32; j++)
-	        snack[i][j].about = snack[i][j].drct = 0;
-	snack[10][16].about = snack[11][16].about = snack[12][16].about = 1;
+	        snack[i][j].about = snack[i][j].dir = UP;
+	snack[10][16].about = snack[11][16].about = snack[12][16].about = SNACK;
+
+	//spawn food
 	srand((unsigned int)time(NULL));
 	for (int i = 0; i < FOODS; i++)
-		make_food();
-	draw_screen();
+		span_food();
+
+	//redraw the screen
+	redr_screen();
 }
 
-void make_food (void)
+void span_food (void)
 {
 	int i = 0;
 	int x, y;
+	//spawn food in a random position that is not occupied by the snack or other food
 	do
 	{
 		x = rand() % 16;
 		y = rand() % 32;
 		i++;
 	}
-	while ( (snack[x][y].about == 1 || snack[x][y].about == 2) && i < 1000);
+	while ( (snack[x][y].about == SNACK || snack[x][y].about == FOOD) && i < 1000);
 	
+	//if the position is valid, place food there
 	if (i < 1000)
-		snack[x][y].about = 2;
+		snack[x][y].about = FOOD;
 }
 
-char get_drct(void)
+char get_dir(void)
 {
+	//if a key is pressed, return the character of the key
 	if(kbhit())
 	    return getch();
 	return 0;
 }
 
-int run (int tdrct)
+int run (int dir)
 {
-	int ret = 0;
-	mem_drct = snack[head.x][head.y].drct = tdrct;
-	int hx = head.x;
-	int hy = head.y;
-	switch(tdrct)
+	int ret = WELL;  //return value: 0-well, 1-geted, 4-over
+	last_dir = snack[head.x][head.y].dir = dir;  //update the direction of the head
+	int head_x_sto = head.x;  //store the previous x coordinate of the head
+	int head_y_sto = head.y;  //store the previous y coordinate of the head
+
+	//update the position of the head based on the direction
+	switch(dir)
 	{
-		case(0):
+		case(UP):
 			head.x--;
 			break;
-		case(1):
+		case(RIGHT):
 			head.y++;
 			break;
-		case(2):
+		case(DOWN):
 			head.x++;
 			break;
-		case(3):
+		case(LEFT):
 			head.y--;
 			break;
 		default:
-			putchar('1');
+			putchar('E1');
 			exit(1);
 	}
-	if ( (snack[head.x][head.y].about == 1  &&  ( head.x != tear.x  ||  head.y != tear.y ) ) || head.x - hx == 15 || hx - head.x == 15 || head.y - hy == 31 || hy - head.y == 31)
+
+	//check if the snack has collided with itself or the wall
+	if ( (snack[head.x][head.y].about == SNACK  &&  ( head.x != tail.x  ||  head.y != tail.y ) ) || head.x - head_x_sto == 15 || head_x_sto - head.x == 15 || head.y - head_y_sto == 31 || head_y_sto - head.y == 31)
     {
     	puts("GAME OVER");
-    	return 4;
+    	return OVER;
 	}
-	if (snack[head.x][head.y].about == 2)
+
+	//check if the snack has eaten food or moved to an empty space
+	if (snack[head.x][head.y].about == FOOD)
 	{
-		snack[head.x][head.y].about = 1;
-		slong++;
-		ret = 1;
-		make_food();
+		snack[head.x][head.y].about = SNACK; //update the position of the head to be occupied by the snack
+		snack_len++;  //increase the length of the snack
+		ret = GETED;  //set the return value to indicate that food has been eaten
+		span_food();  //spawn new food
 	}
-	else if (snack[head.x][head.y].about == 0)
+	//if the snack has moved to an empty space, update the tail position
+	else if (snack[head.x][head.y].about == NULL_)
 	{
-		snack[head.x][head.y].about = 1;
-		snack[tear.x][tear.y].about = 0;
-		switch(snack[tear.x][tear.y].drct)
+		//update the position of the head to be occupied by the snack and the position of the tail to be empty
+		snack[head.x][head.y].about = SNACK;
+		snack[tail.x][tail.y].about = NULL_;
+
+		//update the position of the tail based on its direction
+		switch(snack[tail.x][tail.y].dir)
 		{
-			case(0):
-				tear.x--;
+			case(UP):
+				tail.x--;
 				break;
-			case(1):
-				tear.y++;
+			case(RIGHT):
+				tail.y++;
 				break;
-			case(2):
-				tear.x++;
+			case(DOWN):
+				tail.x++;
 				break;
-			case(3):
-				tear.y--;
+			case(LEFT):
+				tail.y--;
 				break;
 			default:
-				putchar('2');
+				//if the direction is invalid, print an error message and exit
+				putchar('E2');
 				exit(1);
 		}
 	}
-	else
+	else//if (snack[head.x][head.y].about == SNACK)
 	{
-		snack[head.x][head.y].about = 1;
-		switch(snack[tear.x][tear.y].drct)
+		//update the position of the head to be occupied by the snack and the position of the tail to be empty
+		snack[head.x][head.y].about = SNACK;
+		//update the position of the tail based on its direction
+		switch(snack[tail.x][tail.y].dir)
 		{
-			case(0):
-				tear.x--;
+			case(UP):
+				tail.x--;
 				break;
-			case(1):
-				tear.y++;
+			case(RIGHT):
+				tail.y++;
 				break;
-			case(2):
-				tear.x++;
+			case(DOWN):
+				tail.x++;
 				break;
-			case(3):
-				tear.y--;
+			case(LEFT):
+				tail.y--;
 				break;
 			default:
-				putchar('2');
+				//if the direction is invalid, print an error message and exit
+				putchar('E3');
 				exit(1);
 		}
 	}
 	return ret;
 }
 
-int not_drct (int a)
+int opp_dir (int dir)
 {
-	switch(a)
+	//return the opposite direction of the given direction
+	switch(dir)
 	{
-		case(0):
-			return 2;
-		case(1):
-			return 3;
-		case(2):
-			return 0;
-		case(3):
-			return 1;
+		case(UP):
+			return DOWN;
+		case(RIGHT):
+			return LEFT;
+		case(DOWN):
+			return UP;
+		case(LEFT):
+			return RIGHT;
 		default:
-			putchar('3');
+		    //if the direction is invalid, print an error message and exit
+			putchar('E4');
 			exit(1);
 	}
 }
 
-void draw_screen (void)
+void redr_screen (void)
 {
+	//move the cursor to the top-left corner of the console window
 	COORD pos = {0, 0};
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+
+	//clear the buffer and prepare to redraw the screen
 	buffer[0] = 0;
-	char slong_format[5];
-	sprintf(slong_format, "%d", slong);
-	strcat(buffer, " SNACK: ");
-	strcat(buffer, slong_format);
+	//convert the length of the snack to a string and append it to the buffer
+	char snack_len_format[5];
+	sprintf(snack_len_format, "%d", snack_len); //convert the length of the snack to a string
+	strcat(buffer, " SNACK: "); //append the string " SNACK: " to the buffer
+	strcat(buffer, snack_len_format); //append the length of the snack to the buffer
+	//draw the snack and food on the screen
 	strcat(buffer, "\nDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD\n");
 	for(int i = 0; i < 16; i++)
 	{
@@ -253,42 +325,51 @@ void draw_screen (void)
 		{
 			switch(snack[i][j].about)
 			{
-				case(0):
+				case(NULL_): //if the position is empty, append a space to the buffer
 					strcat(buffer, " ");
 					break;
-				case(1):
+				case(SNACK): //if the position is occupied by the snack, append "@" to the buffer
 					strcat(buffer, "@");
 					break;
-				case(2):
+				case(FOOD): //if the position is occupied by food, append "$" to the buffer
 					strcat(buffer, "$");
 					break;
 				default:
-					putchar('4');
+					//if the position is invalid, print an error message and exit
+					putchar('E5');
 					exit(1);
 			}
 		}
 		strcat(buffer, "D\n");
 	}
 	strcat(buffer, "DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD\n");
+	//print the buffer to the console and flush the output
 	printf("%s", buffer);
 	fflush(stdout);
 }
 
-int is_play (void)
+int cont_play (void)
 {
+	//ask the player if they want to continue playing
 	puts("Play again ? (y/n):");
+	//wait for the player to press a key
 	char c = getchar();
+	//if the player wants to continue playing, clear the input buffer and return 1
 	if (c == 'y' || c == 'Y')
 	{
 		while(getchar() != '\n')
 		    continue;
 		return 1;
 	}
+	//if the player does not want to continue playing, clear the input buffer and return 0
 	if (c != '\n')
 		while(getchar() != '\n')
 			continue;
+	
+	//return 0 to indicate that the player does not want to continue playing
 	puts("\n\n-------------------------");
 	puts("Press any key to quit:");
+	//wait for the player to press a key
 	while (kbhit())
 	    Sleep(100);
 	while (!kbhit())
